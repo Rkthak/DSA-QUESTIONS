fbb08379e3b4f38a5c36a78a63adf4d6750e410d
Rkthak <rkon2609@gmail.com>
2026-06-22 17:36:15 +0530

    armstrong number

---
+++ Maths/ArmstrongNumber.js
@@ -0,0 +1,21 @@
+// ! 58 Check if a number is an Armstrong number.
+
+// 1**3 + 5**3 + 3**3 = 1+125+27 = 153
+// 1**4 + 6**4 + 3**4 + 4**4 = 1+1296+81+256 = 1634
+
+let number = 153;
+let digits = number.toString();
+
+let i = 0,
+  sum = 0;
+
+while (i < digits.length) {
+  sum += digits[i] ** digits.length;
+  i++;
+}
+
+if (sum === number) {
+  console.log(`${number} is an Armstrong number`);
+} else {
+  console.log(`${number} is not an Armstrong number`);
+}
