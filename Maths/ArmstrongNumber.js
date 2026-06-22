0ce197af35e14f83cb39181295133dc1eacf1c36
Rkthak <rkon2609@gmail.com>
2026-06-22 17:46:41 +0530

    armstrong number --JS -arr.reduce

--- Maths/ArmstrongNumber.js
+++ Maths/ArmstrongNumber.js
@@ -4,15 +4,22 @@
 // 1**4 + 6**4 + 3**4 + 4**4 = 1+1296+81+256 = 1634
 
 let number = 153;
-let digits = number.toString();
+// let digits = number.toString();
 
-let i = 0,
-  sum = 0;
+// let i = 0,
+//   sum = 0;
 
-while (i < digits.length) {
-  sum += digits[i] ** digits.length;
-  i++;
-}
+// while (i < digits.length) {
+//   sum += digits[i] ** digits.length;
+//   i++;
+// }
+
+// ~ by JAVASCRIPT array method
+let digits = number.toString().split("");
+
+let sum = digits.reduce((acc, curr) => {
+  return Number(acc) + Number(curr ** digits.length);
+});
 
 if (sum === number) {
   console.log(`${number} is an Armstrong number`);
