a3605a92e8e21e69fc4fe946fa25c9b4b1dddb4d
Rkthak <rkon2609@gmail.com>
2026-06-23 14:38:08 +0530

    perfect number --reduced TimeComplexity

--- Maths/perfectNumber.js
+++ Maths/perfectNumber.js
@@ -8,22 +8,41 @@
     28 ==> 1,2,4,7,14,28 ==> 1+2+4+7+14
 */
 
-let number = 28,
-  sumOfProperDivisors = 0;
+// let number = 28,
+//   sumOfProperDivisors = 0;
 
-for (let i = 1; i < number; i++) {
+// for (let i = 1; i < number; i++) {
+//   if (number % i == 0) {
+//     sumOfProperDivisors += i;
+//   }
+// }
+/*
+    Time Complexity = O(n) ==> Loop will run for n-1 times.
+    Space Complexity = O(1)
+
+*/
+
+let number = 28,
+  sumOfProperDivisors = 1;
+
+for (let i = 2; i * i <= number; i++) {
   if (number % i == 0) {
     sumOfProperDivisors += i;
+    if (i !== number / i) {
+      // pairing officer
+      sumOfProperDivisors += number / i;
+    }
   }
 }
 
 if (sumOfProperDivisors === number) {
-  console.log(`${number} is a proper number`);
+  console.log(`${number} is a perfect number`);
 } else {
-  console.log(`${number} is not a proper number`);
+  console.log(`${number} is not a perfect number`);
 }
+
 /*
-    Time Complexity = O(n) ==> Loop will run for n-1 times.
+    Time Complexity = O(√n) ==> loop run for i * i <= number i.e. i <= √number
     Space Complexity = O(1)
 
 */
