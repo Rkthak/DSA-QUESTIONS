5aad5a0247480589097b2ba2a87a1e01443c5758
Rkthak <rkon2609@gmail.com>
2026-06-30 15:02:02 +0530

    fibbonacci series with space complexity O(n)

--- Maths/fibbonacci.js
+++ Maths/fibbonacci.js
@@ -4,16 +4,34 @@
   n = 10  series: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 */
 
-let n = 10,
-  series = [0, 1];
+// let n = 10,
+//   series = [0, 1];
 
-for (let i = 0; i < n - 2; i++) {
-  series.push(series[i] + series[i + 1]);
-}
+// for (let i = 0; i < n - 2; i++) {
+//   series.push(series[i] + series[i + 1]);
+// }
 
-console.log(series.join(", "));
+// console.log(series.join(", "));
 
 /* 
     Time Complexity = O(n)
     Space Complexity = O(n) ===> depends array length
+*/
+
+let n = 10,
+  firstTerm = 0,
+  secondTerm = 1,
+  nextSum = 0;
+
+for (let i = 0; i < n; i++) {
+  console.log(nextSum);
+  nextSum = firstTerm + secondTerm;
+
+  secondTerm = firstTerm;
+  firstTerm = nextSum;
+}
+
+/* 
+  Time Complexity = O(n)
+  Space Complexity = O(1)
 */
